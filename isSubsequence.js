2c76a79e39a88d8951f242ee7810e7c899d6c21d
Andrew Fuller <fuller2580@gmail.com>
2022-12-06 15:40:50 -0500

    isSubsequence refactor with pointers

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,20 +1,13 @@
 var isSubsequence = function(s, t) {
   let sArr = s.split('');
   let tArr = t.split('');
-  let isSub = true;
-  let lastCharPos;
-  sArr.forEach(char => {
-      if(!t.includes(char)) {
-          isSub = false;
-      } else {
-          if (lastCharPos === undefined) {
-              lastCharPos = tArr.indexOf(char);
-          } else if (tArr.indexOf(char) > lastCharPos) {
-              lastCharPos = tArr.indexOf(char);
-          } else {
-              isSub = false;
-          }
-      }
-  })
-  return isSub;
+  let sPointer = 0;
+  let tPointer = 0;
+  while (sPointer < sArr.length && tPointer < tArr.length) {
+    if (sArr[sPointer] === tArr[tPointer]) {
+      sPointer++;
+    }
+    tPointer++;
+  }
+  return sPointer === sArr.length;
 };
